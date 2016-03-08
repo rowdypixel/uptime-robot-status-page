8734baf4c7433b25be89efcfebaf1004caa9fa98
Dan Walker <dan@rowdypixel.com>
2016-03-08 17:49:21 -0500

    Start adding styles

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 $(function() {
     var apiKeys = [
         'm776869201-5fb62143a517518855535060', // Beef calc
-        'm776200798-7f01a10f2fa63a5e45f5b05b' // personal site
+        //'m776200798-7f01a10f2fa63a5e45f5b05b' // personal site
     ];
+    
+    var anySitesDown = false;
 
     $(apiKeys).each(function() {
         var url = 'https://api.uptimerobot.com/getMonitors?logs=1&alertContacts=1&responseTimes=1&statuses=1-2&responseTimesAverage=180&customUptimeRatio=30&format=json&noJsonCallback=1&apiKey=' + this;
@@ -20,9 +22,21 @@ $(function() {
             }
 
             else {//if (monitor.status == '1') {
-
+                anySitesDown = true;
+                $.get('/tpl/down.html', null, function(result) {
+                    
+                    var finalTemplate = result;
+                    finalTemplate = finalTemplate.replace("{{monitor.name}}", monitor.friendlyname);
+                    finalTemplate = finalTemplate.replace("{{monitor.uptime}}", monitor.alltimeuptimeratio);
+                    $('#results').append(finalTemplate);
+                });
             }
         });
     });
+    
+    if(anySitesDown)
+        $('.overall-status').addClass("status-error");
+    else
+        $('.overall-status').addClass("status-operational");
 });
 

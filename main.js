8dbc54b32dbff59a56d6ed2e19b6b1bcec35404c
Dan Walker <dan@rowdypixel.com>
2016-03-08 16:53:37 -0500

    Add jQuery

--- main.js
+++ main.js
@@ -2,9 +2,8 @@
     var apiKey = 'u147240-04ca80f5f67800d222f88b33';
     var url = 'https://api.uptimerobot.com/getMonitors?logs=1&alertContacts=1&responseTimes=1&responseTimesAverage=180&monitors=15830-32696&customUptimeRatio=30&format=json&apiKey = ' + apiKey;
     
-    $.get(url).success(function()
-    {
-        
+    $.getJson(url, function(result){
+        console.log(result);
     });
 });
 

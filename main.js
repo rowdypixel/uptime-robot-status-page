bfe56a808d2dd767bccc5a1448a97125e0738c3f
Dan Walker <dan@rowdypixel.com>
2016-03-08 22:39:07 -0500

    Wait until all sites are loaded to show the overall status. Add ability to show sites in multiple categories.

--- main.js
+++ main.js
@@ -1,41 +1,60 @@
 $(function() {
-    var apiKeys = [
+    var appApiKeys = [
         'm776869201-5fb62143a517518855535060', // Beef calc
         //'m776200798-7f01a10f2fa63a5e45f5b05b' // personal site
     ];
 
     var anySitesDown = false;
 
-    $(apiKeys).each(function() {
-        var url = 'https://api.uptimerobot.com/getMonitors?logs=1&alertContacts=1&responseTimes=1&statuses=1-2&responseTimesAverage=180&customUptimeRatio=30&format=json&noJsonCallback=1&apiKey=' + this;
-        $.getJSON(url, function(result) {
-            var monitor = result.monitors.monitor[0];
-            console.log(monitor);
-            loadTplForMonitor(monitor);
-        });
+    loadMonitorsInCategory(appApiKeys, '#apps');
+    loadMonitorsInCategory(appApiKeys, '#sites');
+
+    $(document).ajaxStop(function() {
+        console.log($('all-sites-operational').val());
+        $('.overall-status').removeClass('status-intermediate');
+        if (anySitesDown) {
+            $('.overall-status').addClass("status-error");
+            $('.overall-status .site-summary').html('There is a disruption in service.');
+        }
+        else {
+            $('.overall-status').addClass("status-operational");
+            $('.overall-status .site-summary').html('All systems available.');
+        }
     });
 
-    if (anySitesDown)
-        $('.overall-status').addClass("status-error");
-    else
-        $('.overall-status').addClass("status-operational");
 
-    function loadTplForMonitor(monitor) {
+    function loadMonitorsInCategory(apiKeys, categorySelector) {
+        $(apiKeys).each(function() {
+            var url = 'https://api.uptimerobot.com/getMonitors?logs=1&alertContacts=1&responseTimes=1&statuses=1-2&responseTimesAverage=180&customUptimeRatio=30&format=json&noJsonCallback=1&apiKey=' + this;
+            $.getJSON(url, function(result) {
+                var monitor = result.monitors.monitor[0];
+                loadTplForMonitor(monitor, categorySelector);
+            });
+        });
+    }
+
+    function loadTplForMonitor(monitor, categorySelector) {
         var path = '/tpl/up.html';
-        if (monitor.status != 2)
+        if (monitor.status != 2) {
             path = '/tpl/down.html';
+            anySitesDown = true;
+        }
 
         $.get(path, null, function(result) {
             var finalTemplate = result;
             finalTemplate = finalTemplate.replace("{{monitor.name}}", monitor.friendlyname);
             finalTemplate = finalTemplate.replace("{{monitor.uptime}}", monitor.alltimeuptimeratio);
-            $('#results').append(finalTemplate);
+            $(categorySelector).append(finalTemplate);
 
-            $('.site-status .site-summary').click(function() {
-                $(this).siblings('.site-details').toggle();
+            $('.site.site-status .site-summary').unbind('click');
+            $('.site.site-status .site-summary').click(function() {
+                $(this).parents().children('.site-details').toggleClass('hidden');
+                $(this).toggleClass('rounded');
+                $(this).toggleClass('rounded-top');
             });
 
         });
     }
+
 });
 
